7f083e758952e9e929a064676cd7bba3af67e2e3
Paulo <wvictor3011@gmail.com>
2023-07-10 17:52:39 -0300

    implementing Controller methods

--- src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -1,4 +1,25 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.domain.filme.DadosCadastroFilme;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/filmes")
 public class FilmeController {
+
+    @GetMapping
+    public String carregaPaginaFormulario() {
+        return "filmes/formulario";
+}
+
+
+    @PostMapping
+    public  String cadastraFilme(DadosCadastroFilme dados) {
+        System.out.println(dados);
+            return "filmes/formulario";
+    }
+
 }
